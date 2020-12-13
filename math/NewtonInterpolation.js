e11ff6f459aef06112249f528139921cfb5b2e71
UECAndor <65871774+UECAndor@users.noreply.github.com>
2020-12-13 17:47:09 +0100

    Bug fix
    Making some changes to the second 2 dimensiol array having some bugs with higher numbers

--- math/NewtonInterpolation.js
+++ math/NewtonInterpolation.js
@@ -4,10 +4,11 @@ new function Interpolation (x, y, a, n) {
     var y = []
     var a = []
     var n = Number
-    var f = []
-    var i = 0
-    var j = 0
-    var m = []
+
+    let f = []
+    let i = 0
+    let j = 0
+    let m = []
 
     a = a.map(function() { return 0; })     //empty current coefficent of the polynom
     x = x.map(parseFloat)                   //format real values for calculation
@@ -15,8 +16,21 @@ new function Interpolation (x, y, a, n) {
     f[0] = []                               //add new array to the 2. Dimension
 
     /*
-        Generate the newton interpolation pyramid with a 2 Dimensiol array of the function value f.
+        Description of the function:
+        This function uses the newton interpolation to get the coefficents of a polinomial back. 
+        As input you enter 2 arrays of numbers or string representing the x and y values of the term, an array, where you store the coefficients 
+        and the degree of the polinomial. 
+        At first you will prepare the values for further use in the code. 
+        There after you will check for valid inputs.
+        Then you build the interpolation pyramid with the function values array f. 
+        Afterwards you multiply the binominal forms m.
+        Then you multipy and add the values of f and m to get back the array of a in its final form.
+
         https://en.wikipedia.org/wiki/Newton_polynomial
+    */
+
+    /*
+        Generate the newton interpolation pyramid with a 2 Dimensiol array of the function value f.
 
                 f[0][]              f[1][]                                     f[2][]                                  f[3][]                               f[4][]
 
@@ -49,9 +63,9 @@ new function Interpolation (x, y, a, n) {
         Generate a 2 dimensiol array for storing the binomial values of the x-Array
 
                 m[0][]      m[1][]      m[2][]                          m[3][]
-        1       1           -x[0]       x[0]x[1]                        -(x[0]x[1]x[2])
-        x                   1           m[1][0] + m[1][1] * x[1]        m[2][0] + m[2][1] * x[2]
-        x^2                             1                               -(m[2][1] + m[2][2] * x[2])
+        x^0     1           -x[0]       x[0]x[1]                        x[0]x[1]x[2]
+        x                   1           m[1][0] + m[1][1] * -x[1]       m[2][0] + m[2][1] * -x[2]
+        x^2                             1                               m[2][1] + m[2][2] * -x[2]
         x^3                                                             1
         x^n
    */
@@ -66,23 +80,18 @@ new function Interpolation (x, y, a, n) {
    for(i = 2; i <= n + 1; i++ ){
         //Init root value of the dimension
         m[i] = []
-        m[i][0] = m[i - 1][0] * x[i - 1]
+        m[i][0] = m[i - 1][0] * -x[i - 1]
 
         //Loop over all sub dimensions of the root
         for(j = 1; j < i; j++){
-            if(i%2 == 0 && j%2 !== 0){
-                m[i][j] = m[i - 1][j - 1] + m[i - 1][j] * x[i - 1]
-            }
-            else{
-                m[i][j] = m[i - 1][j - 1] + m[i - 1][j] * x[i - 1] * -1
-            }
+            m[i][j] = m[i - 1][j - 1] + m[i - 1][j] * -x[i - 1]
         }
         m[i][j] = 1
     }
 
     //Calculate final coefficents by multipling the 2 dimensiol pyramid array f and the 2 dimensiol array of binomial x values m
     for(i = 0; i < n; i++){
-
+        a[i] = 0
         for(j = i; j <= n; j++){
             a[i] += m[j][i] * f[j][0]
         }
